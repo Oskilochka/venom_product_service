4a62ba808200e044568c6b1d970c6b3de5941633
Julia Oskilko <yulia.osk14@gmail.com>
2024-09-29 12:54:18 +0200

    add product controller tests

--- src/main/java/com/josk/venom/model/Product.java
+++ src/main/java/com/josk/venom/model/Product.java
@@ -8,10 +8,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/josk/venom/repository/ProductRepository.java
+++ src/main/java/com/josk/venom/repository/ProductRepository.java
@@ -1,13 +1,6 @@
 package com.josk.venom.repository;
 
 import com.josk.venom.model.Product;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
